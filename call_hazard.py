7084a3383a3f8b1a955940dbf84d306f30dace99
João Bosco Martinolli <joaoboscojbm@gmail.com>
2024-10-13 14:42:00 +0400

    Commit_12

--- call_hazard.py
+++ call_hazard.py
@@ -7,13 +7,33 @@ Created on Thu Aug 17 15:50:08 2023
 from main_hazard import create_hazard
 
 
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 15:50:08 2023
+
+@author: Joao.Bosco
+"""
+from main_hazard import create_hazard
+
 def hazard_function():
-    # Collect hazard details
-    new_hazard = ""
-    with open("hazards.txt", "a") as f:
-        f.write(input("Please enter the Title of your File: ") + "\n")
+    try:
+        print("Starting hazard function...")
+        # Collect hazard details
+        new_hazard = ""
+        with open("hazards.txt", "a") as f:
+            title = input("Please enter the Title of your file: ")
+            f.write(title + "\n")
+            print(f"Title '{title}' written to file!\n")
+        
+            new_hazard = create_hazard()
+            f.write(str(new_hazard) + "\n")
+            print(f"Hazard '{new_hazard}' written to file!\n")
+                    
+        print("Hazard details written to file successfully!")
+        return new_hazard
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-        new_hazard = create_hazard()
-
-        f.write(str(new_hazard) + "\n")
-    return new_hazard
+# Call the function to test
+hazard_function()
+            
